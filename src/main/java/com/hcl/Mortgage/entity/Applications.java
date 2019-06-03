d7cc2c17b64c8738f7184b532a989d0e372431c5
nithin2889 <nithin2889@gmail.com>
2019-06-03 12:56:30 +0530

    application entity creation

---
+++ src/main/java/com/hcl/Mortgage/entity/Applications.java
@@ -0,0 +1,78 @@
+package com.hcl.Mortgage.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+@Entity
+@Table
+@SequenceGenerator(name="application_number_seq", initialValue=21234351, allocationSize=100)
+public class Applications {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="account_number_seq")	
+ private Long id;	
+ private String firstName;
+ private String lastName;
+ private String address;
+ private String creditStatus;
+ private Double applicantIncome;
+ private Double loanAmount;
+ private Date timeCreated;
+ 
+ 
+ 
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getCreditStatus() {
+	return creditStatus;
+}
+public void setCreditStatus(String creditStatus) {
+	this.creditStatus = creditStatus;
+}
+public Double getApplicantIncome() {
+	return applicantIncome;
+}
+public void setApplicantIncome(Double applicantIncome) {
+	this.applicantIncome = applicantIncome;
+}
+public Double getLoanAmount() {
+	return loanAmount;
+}
+public void setLoanAmount(Double loanAmount) {
+	this.loanAmount = loanAmount;
+}
+public Date getTimeCreated() {
+	return timeCreated;
+}
+public void setTimeCreated(Date timeCreated) {
+	this.timeCreated = timeCreated;
+}
+ 
+}
+
