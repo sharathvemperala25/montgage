22eb3aad0a7c7ad3fd84e95c4439740d89f74eb2
nithin2889 <nithin2889@gmail.com>
2019-06-03 15:46:18 +0530

    save

--- src/main/java/com/hcl/Mortgage/controller/ApplicationController.java
+++ src/main/java/com/hcl/Mortgage/controller/ApplicationController.java
@@ -2,10 +2,12 @@ package com.hcl.Mortgage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.hcl.Mortgage.entity.Applications;
 import com.hcl.Mortgage.service.ApplicationService;
 
 @RestController
@@ -19,6 +21,12 @@ public class ApplicationController {
 	public String getApproval(@PathVariable("id") Long  appId)
 	{
 		return applicationService.approveCredit(appId);
+	}
+	
+
+	@PostMapping("/createApplication")
+	public Applications createApplication(@RequestBody Applications applications) {
+		return applicationService.saveApplication(applications);
 	}
 
 }
